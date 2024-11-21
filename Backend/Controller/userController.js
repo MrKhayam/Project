227d9b3fc7d643594d7888a0128711bbe6d57623
unknown <ijazkhayam@gmail.com>
2024-11-22 03:15:53 +0500

    update

--- Backend/Controller/userController.js
+++ Backend/Controller/userController.js
@@ -1,27 +1,44 @@
-const User = require("../Models/userModel")
+const User = require("../Models/userModel");
+const bcrypt = require("bcrypt");
 
 const asyncHandler = require("express-async-handler");
 
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, dob, password } = req.body;
+  const checkEmail = await User.findOne({ email });
+  if (checkEmail) {
+    throw new Error("Email already registered!");
+  }
 
-const registerUser = asyncHandler( async (req, res) => {
-    const { name, email, dob, password } = req.body;
-    if (name && email && dob && password) {
-        try {
-            const createdUser = await User.create({
-              name,
-              email,
-              dob,
-              password,
-            });
+  if (name && email && dob && password) {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPass = await bcrypt.hash(password, salt);
+      const createdUser = await User.create({
+        name,
+        email,
+        dob,
+        password: hashedPass,
+      });
 
-            res.send(createdUser)
-        } catch (error) {
-            console.log(error);
-        }
+      res.send(createdUser);
+    } catch (error) {
+      console.log(error);
     }
-    else {
-        throw new Error("Please fill all the fields.");
-    }
+  } else {
+    throw new Error("Please fill all the fields.");
+  }
 });
 
-module.exports = { registerUser };
+
+
+
+const loginUser = (req,res) => {
+    res.send("User Logged In");
+}
+
+
+
+
+
+module.exports = { registerUser, loginUser };
--- Backend/Routes/userRoute.js
+++ Backend/Routes/userRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { registerUser } = require("../Controller/userController");
+const { registerUser, loginUser } = require("../Controller/userController");
 
 const router = express.Router();
 
 router.post("/register-user", registerUser);
+router.post("/login-user", loginUser);
 
 module.exports = router;
