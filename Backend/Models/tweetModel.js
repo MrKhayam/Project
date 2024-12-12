29ac74eb2f2356224f238ef729c4010d577d4b85
Khayam Ijaz <ijazkhayam@gmail.com>
2024-12-12 11:24:07 +0500

    update

--- Backend/Models/tweetModel.js
+++ Backend/Models/tweetModel.js
@@ -2,5 +2,21 @@ const mongoose = require('mongoose');
 
 
 const tweetSchema = mongoose.Schema({
-    
-})
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required: true,
+    },
+    caption:{
+        type:String,
+        required: false,
+    },
+    content:{
+        type:String,
+        required:false,
+    }
+},{
+    timpestamps:true,
+});
+
+module.exports = mongoose.model('Tweet',tweetSchema);
